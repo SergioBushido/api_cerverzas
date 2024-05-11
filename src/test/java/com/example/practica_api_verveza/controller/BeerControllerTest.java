0ad4eaedaf64e7c4c4b0395738c8954eee96ffc4
Sergio <sergio.ume@hotmail.com>
2024-05-11 17:29:18 +0100

    test beercontroler jsonPath

--- src/test/java/com/example/practica_api_verveza/controller/BeerControllerTest.java
+++ src/test/java/com/example/practica_api_verveza/controller/BeerControllerTest.java
@@ -51,4 +51,20 @@ class BeerControllerTest {
 
 
     }
-}
+
+    @Test
+    void getListBeers() throws Exception {
+
+        given(beerService.listBeers()).willReturn(beerServiceImpl.listBeers());
+
+        mockMvc.perform(get("/api/v1/beer")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()",is(3)));
+    }
+
+
+
+
+    }
