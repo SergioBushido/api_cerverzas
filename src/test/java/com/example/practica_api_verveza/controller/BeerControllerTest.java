eb267eadc1ae8ebfcbf9883fa48167a285195c28
sergio <sergio.ume@hotmail.com>
2024-05-13 15:51:56 +0100

    test beer

--- src/test/java/com/example/practica_api_verveza/controller/BeerControllerTest.java
+++ src/test/java/com/example/practica_api_verveza/controller/BeerControllerTest.java
@@ -3,6 +3,9 @@ package com.example.practica_api_verveza.controller;
 import com.example.practica_api_verveza.model.Beer;
 import com.example.practica_api_verveza.services.BeerService;
 import com.example.practica_api_verveza.services.BeerServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,12 +13,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.UUID;
 import static org.hamcrest.core.Is.is;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.web.servlet.function.RequestPredicates.contentType;
@@ -30,11 +35,44 @@ class BeerControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @MockBean
     BeerService beerService;
 
-    BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+    BeerServiceImpl beerServiceImpl;
 
+    @BeforeEach
+    void setUp(){
+        beerServiceImpl = new BeerServiceImpl();
+    }
+
+    //objectMapper de json a objeto y viceversa
+    //findAndRegisterModules busca modulos para conversion de fecha hora por ej.
+    @Test
+    void testCreateNewBerr() throws Exception {
+        //De esta manera el formato fecha hora es distinto que utilzando  el autowired
+        //ObjectMapper objectMapper = new ObjectMapper();
+        //objectMapper.findAndRegisterModules();
+        //Beer beer = beerServiceImpl.listBeers().get(0);
+        //System.out.println(objectMapper.writeValueAsString(beer));
+
+        Beer beer = beerServiceImpl.listBeers().get(0);
+        beer.setVersion(null);
+        beer.setId(null);
+
+        given(beerService.saveNewBeer(any(Beer.class))).willReturn(beerServiceImpl.listBeers().get(1));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/beer")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(beer)))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"));
+
+
+    }
 
     @Test
     void getBeerById() throws Exception {
