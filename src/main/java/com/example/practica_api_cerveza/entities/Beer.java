9ba6bf1146b32379f5003523b54982a20b4d0997
Sergio <sergio.ume@hotmail.com>
2024-05-19 20:43:00 +0100

    Dockerizado con mysql/se han modificado las entidades para que funcione mysql

--- src/main/java/com/example/practica_api_cerveza/entities/Beer.java
+++ src/main/java/com/example/practica_api_cerveza/entities/Beer.java
@@ -1,4 +1,4 @@
-package com.example.practica_api_cerveza.entities;
+/*package com.example.practica_api_cerveza.entities;
 
 import com.example.practica_api_cerveza.model.BeerStyle;
 import jakarta.persistence.*;
@@ -46,6 +46,66 @@ public class Beer {
 
     @NotNull
     private BigDecimal price;
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+}
+*/
+package com.example.practica_api_cerveza.entities;
+
+import com.example.practica_api_cerveza.model.BeerStyle;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "beer")
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(columnDefinition = "binary(16)", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Integer version;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 50)
+    @Column(length = 50, nullable = false)
+    private String beerName;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private BeerStyle beerStyle;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 255)
+    @Column(length = 255, nullable = false)
+    private String upc;
+
+    private Integer quantityOnHand;
+
+    @NotNull
+    @Column(precision = 38, scale = 2, nullable = false)
+    private BigDecimal price;
+
     private LocalDateTime createDate;
     private LocalDateTime updateDate;
 }
--- src/main/java/com/example/practica_api_cerveza/entities/Customer.java
+++ src/main/java/com/example/practica_api_cerveza/entities/Customer.java
@@ -1,4 +1,4 @@
-package com.example.practica_api_cerveza.entities;
+/*package com.example.practica_api_cerveza.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
@@ -26,5 +26,41 @@ public class Customer {
     @Version
     private Integer version;
     private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}*/
+package com.example.practica_api_cerveza.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(columnDefinition = "binary(16)", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "name", length = 255)
+    private String name;
+
+    @Version
+    private Integer version;
+
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+
+    @Column(name = "update_date")
     private LocalDateTime updateDate;
 }
